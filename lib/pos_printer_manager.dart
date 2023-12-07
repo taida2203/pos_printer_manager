58724822f1b974be73bd1f30c9664131d40c2c07
toby <toby.dao@techvify.com.vn>
2023-12-07 19:39:01 +0700

    update esc_pos_utils_plus.dart

--- lib/pos_printer_manager.dart
+++ lib/pos_printer_manager.dart
@@ -9,7 +9,7 @@ export 'package:pos_printer_manager/services/usb_printer_manager.dart';
 export 'package:pos_printer_manager/enums/bluetooth_printer_type.dart';
 export 'package:pos_printer_manager/enums/connection_response.dart';
 export 'package:pos_printer_manager/enums/connection_type.dart';
-export 'package:esc_pos_utils_plus/esc_pos_utils.dart';
+export 'package:esc_pos_utils_plus/esc_pos_utils_plus.dart';
 
 class PosPrinterManager {
   static EasyLogger logger = EasyLogger(
